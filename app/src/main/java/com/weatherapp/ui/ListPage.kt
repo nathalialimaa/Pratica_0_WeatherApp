ec742525fcd52e5e89931c97bb72a934a7c2394a
Nathália de Lima <nathylima464@gmail.com>
2026-05-28 15:04:56 -0300

    Prática 3 - Parte 1(Criando uma lista simples)

--- app/src/main/java/com/weatherapp/ui/ListPage.kt
+++ app/src/main/java/com/weatherapp/ui/ListPage.kt
@@ -1,45 +1,124 @@
 package com.weatherapp.ui
+
 import android.app.Activity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.weatherapp.model.City
 
+private fun getCities() = List(20) { i ->
+    City(
+        name = "Cidade $i",
+        weather = "Carregando clima..."
+    )
+}
 
 @Composable
 fun ListPage(modifier: Modifier = Modifier) {
 
-    Column(
+    val cityList = remember {
+        getCities().toMutableStateList()
+    }
+
+    val activity = LocalContext.current as Activity
+
+    LazyColumn(
         modifier = modifier
             .fillMaxSize()
-            .background(Color.Magenta)
-            .wrapContentSize(Alignment.Center)
+            .padding(8.dp)
     ) {
 
-        Text(
-            text = "Favoritas",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+        items(cityList, key = { it.name }) { city ->
+
+            CityItem(
+
+                city = city,
+
+                onClick = {
+
+                    Toast.makeText(
+                        activity,
+                        "Cidade: ${city.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                },
+
+                onClose = {
+
+                    Toast.makeText(
+                        activity,
+                        "Remover: ${city.name}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun CityItem(
+    city: City,
+    onClick: () -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { onClick() },
+
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
         )
+
+        Spacer(modifier = Modifier.size(12.dp))
+
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+
+            Text(
+                text = city.name,
+                fontSize = 24.sp
+            )
+
+            Text(
+                text = city.weather ?: "Carregando clima...",
+                fontSize = 16.sp
+            )
+        }
+
+        IconButton(onClick = onClose) {
+
+            Icon(
+                Icons.Filled.Close,
+                contentDescription = "Close"
+            )
+        }
     }
 }
